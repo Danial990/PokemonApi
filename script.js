b8107d8355e3e0a4a21adc1605b99eb9c9abce03
Danial Hazrati <dhazrati1990@gmail.com>
2022-06-19 12:20:30 +0200

    tasks done!

--- script.js
+++ script.js
@@ -1,118 +1,142 @@
 const template = document.querySelector("[data-user-template]")
-const templateCards = document.querySelector("[data-pokemon-cards-container]")
-const searchInput = document.querySelector("[data-search]")
-const props = [];
+const domCards = document.querySelector("[data-pokemon-cards-container]")
+const inputBox = document.querySelector("[data-search]")
+const searchWrapper = document.querySelector(".search-input")
+const suggBox = document.querySelector(".autocom-box")
 
-let datas;
-var pokemonsData = [];
-let wholeData = {};
-
-searchInput.addEventListener("input", (e) => {
-    const value = e.target.value;
-
-    if (value) {
-        pokemonsData.forEach(data => {
-            const isVisible = data.name.includes(value);
-            data.element.classList.toggle("hide", isVisible);
-        })
-    } else {
-        pokemonsData.forEach(data => {
-            data.element.classList.remove("hide");
-        })
-    }
-})
+let currentPokemon = {};
+let pokeItems = [];
+let searchResult = [];
+let liList = [];
 
 
+inputBox.addEventListener("input", (e) => {
+    const search = e.target.value;
 
-//reading each pokemons datas link 1~20 and saving their infos in an array
-function test() {
-    for (let index = 1; index <= 20; index++) {
-        fetch('https://pokeapi.co/api/v2/pokemon/' + `${index}`)
-            .then(results => results.json())
-            .then(infos => {
-                const personlyHeight = infos.height;
-                const personlyWeight = infos.weight;
-                const personlyName = infos.name;
-                const personlyAbility = infos.abilities;
-                const personlyType = infos.types[0].type.name;
-                const image = '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/' + `${index}` + '.png" />'
+    if (search) {
+        fetch('https://pokeapi.co/api/v2/pokemon/')
+            .then(res => res.json())
+            .then(data => {
+                pokeItems = data.results;
+                const searchRes = pokeItems.filter(poke => poke.name.startsWith(search))
+                searchResult = searchRes;
 
-                let t1 = '';
-                if (infos.types[1]) {
-                    t1 = ', ' + infos.types[1].type.name;
-                }
+                const pokeNames = searchResult.map(data => {
+                    return data.name;
+                });
+
+                liList = pokeNames.map(list => {
+                    return '<li>' + list + '</li>';
+                });
+
+                suggBox.innerHTML = liList;
+
+                showSuggestion(liList);
 
 
-                const v = infos.abilities[0].ability.name;
+                let allList = suggBox.querySelectorAll("li");
+                searchWrapper.classList.add("active"); //show auto complete Box
 
-                let v1 = '';
-                if (infos.abilities[1]) {
-                    v1 = ', ' + infos.abilities[1].ability.name;
-                }
+                allList.forEach(el => {
+                    el.addEventListener("click", select)
+                })
+            })
 
-                let v2 = '';
-                if (infos.abilities[2]) {
-                    v2 = ', ' + infos.abilities[2].ability.name;
-                }
-
-                // let v = []
-                // for (let [prop, value] of Object.entries(infos.abilities)) {
-                //     v[props] = value.ability.name;
-                // }
-                // console.log(v);
-
-                props[index - 1] = { personlyHeight, personlyWeight, personlyName, personlyAbility, personlyType, t1, v, v1, v2, image };
+            .catch(err => {
+                console.error(err)
             })
     }
-    // console.log(props);
-    return props;
+    else {
+        searchWrapper.classList.remove("active"); //hide auto complete Box
+    }
+});
+
+// clicking on suggested pokemon
+function select(e) {
+    cleanUpCard();
+    const selectedName = e.target.textContent;
+    const selectedPokemon = searchResult.find(data => data.name === selectedName)
+
+    const link = selectedPokemon.url;
+    readPokemonsProps(link);
+    inputBox.value = selectedName;
 }
 
-test();
+
+function showSuggestion(list) {
+    let listData;
+    listData = list.join('');
+    suggBox.innerHTML = listData;
+}
+
+//reading selected Pokemon
+function readPokemonsProps(url) {
+
+    fetch(url)
+        .then(results => results.json())
+        .then(infos => {
+            const personlyHeight = infos.height;
+            const personlyWeight = infos.weight;
+            const personlyName = infos.name;
+            const personlyAbility = infos.abilities;
+            const personlyType = infos.types;
+
+            const image = infos.sprites.back_default;
+
+            let pokesType = [];
+            pokesType = personlyType.map(el => {
+                return pokesType.push = el.type.name;
+            });
+
+
+            let pokesAbility = [];
+            pokesAbility = personlyAbility.map(el => {
+                return pokesAbility.push = el.ability.name;
+            });
+
+
+            currentPokemon = { personlyHeight, personlyWeight, personlyName, pokesAbility, pokesType, image };
+            appendPokemonsCard(currentPokemon);
+        })
+        .catch(err => {
+            console.error(err)
+        })
+}
 
 
 // reading main link
-fetch('https://pokeapi.co/api/v2/pokemon/')
+function appendPokemonsCard(currentPokemon) {
+    const card = template.content.cloneNode(true).children[0];
+    const header = card.querySelector("[data-header]");
+    const img = card.querySelector("[data-img]");
+    const type = card.querySelector("[data-type-body]");
+    const height = card.querySelector("[data-height]");
+    const weight = card.querySelector("[data-weight]");
+    const abilities = card.querySelector("[data-abilities]");
 
-    .then(res => res.json())
-    .then(data => {
-        pokemonsData = data.results.map(user => {
-            // selecting template elements
-            const card = template.content.cloneNode(true).children[0];
-            const header = card.querySelector("[data-header]");
-            const img = card.querySelector("[data-img]");
-            const type = card.querySelector("[data-type-body]");
-            const height = card.querySelector("[data-height]");
-            const weight = card.querySelector("[data-weight]");
-            const abilities = card.querySelector("[data-abilities]");
+    header.textContent = currentPokemon.personlyName;
+    height.textContent = 'height: ' + currentPokemon.personlyHeight;
+    weight.textContent = 'weight: ' + currentPokemon.personlyWeight;
+    type.textContent = [...currentPokemon.pokesType];
+    abilities.textContent = 'abilities: ' + [...currentPokemon.pokesAbility]
+    img.src = currentPokemon.image;
 
-            let c = 0;
-            // assigning the data of each Pokemon to its name selected above.
-            props.map(arr => {
+    card.classList.add("show");
+    domCards.append(card);
 
-                if (arr.personlyName == user.name) {
-                    header.textContent = user.name;
-                    height.textContent = 'height: ' + props[c].personlyHeight;
-                    weight.textContent = 'weight: ' + props[c].personlyWeight;
-                    type.textContent = props[c].personlyType + ' ' + props[c].t1;
-                    abilities.textContent = 'abilities: ' + props[c].v + ' ' + props[c].v1 + ' ' + props[c].v2;
-                    img.innerHTML = props[c].image;
-                    datas = {
-                        name: header.textContent,
-                        hegiht: height.textContent,
-                        weight: weight.textContent,
-                        type: type.textContent,
-                        abilities: abilities.textContent
-                    }
-                } else {
-                    c++;
-                }
-                templateCards.append(card);
-                return wholeData = { ...datas, element: card };
-            });
-            // console.log(pokemonsData);
-            return pokemonsData = wholeData;
-        });
-    })
-// });
+    closeButton();
+}
 
+function cleanUpCard() {
+    const allCards = document.querySelectorAll(".pokemon-cards")
+    if (allCards.length != 0) {
+        allCards.forEach(element => {
+            element.remove();
+        })
+    }
+}
+
+function closeButton() {
+    const xButton = document.getElementById("close");
+    xButton.addEventListener("click", cleanUpCard)
+}
